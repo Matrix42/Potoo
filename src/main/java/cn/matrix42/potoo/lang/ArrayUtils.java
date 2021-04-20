68c4f17a0cd7205feacba35229c63be384a62e21
Matrix42 <934336389@qq.com>
2021-04-20 22:00:31 +0800

    [lang] add ArrayUtils

--- src/main/java/cn/matrix42/potoo/lang/ArrayUtils.java
+++ src/main/java/cn/matrix42/potoo/lang/ArrayUtils.java
@@ -1,7 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.matrix42.potoo.lang;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Random;
 
 import cn.matrix42.potoo.annotation.NotNull;
 import cn.matrix42.potoo.annotation.Nullable;
@@ -31,6 +49,26 @@ public class ArrayUtils {
             newArray[i] = array[indexes[i]];
         }
         return newArray;
+    }
+
+    public static <T> void knuthShuffle(@NotNull T[] array) {
+        Preconditions.checkNotNull(array);
+        Random random = new Random();
+        int n = array.length;
+        for (int i = 0; i < array.length; i++) {
+            int t = i + random.nextInt() % (n - i);
+            //每次随机出i~n-1之间的下标
+            swap(array, i, t);
+        }
+    }
+
+    public static <T> void swap(@NotNull T[] array, int index1, int index2) {
+        Preconditions.checkNotNull(array);
+        Preconditions.checkArgument(index1 < array.length);
+        Preconditions.checkArgument(index2 < array.length);
+        T tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
     }
 
     /**
