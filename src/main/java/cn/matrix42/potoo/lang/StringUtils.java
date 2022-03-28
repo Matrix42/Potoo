481b713d324b39716138c37570ac1f3182cd12b9
Matrix42 <934336389@qq.com>
2022-03-28 11:23:49 +0800

    [lang] add join repeat for StringUtils

--- src/main/java/cn/matrix42/potoo/lang/StringUtils.java
+++ src/main/java/cn/matrix42/potoo/lang/StringUtils.java
@@ -17,11 +17,17 @@
 
 package cn.matrix42.potoo.lang;
 
+import java.util.Iterator;
+
 /**
  * @author Matrix42
  * @since 1.0.0
  */
 public class StringUtils {
+
+    public static final String EMPTY = "";
+
+    private static final int PAD_LIMIT = 8192;
 
     private StringUtils() {
 
@@ -43,6 +49,93 @@ public class StringUtils {
      */
     public static boolean isNotEmpty(String str) {
         return !StringUtils.isEmpty(str);
+    }
+
+    public static String join(final Iterator<?> iterator, final String separator) {
+
+        // handle null, zero and one elements before building a buffer
+        if (iterator == null) {
+            return null;
+        }
+        if (!iterator.hasNext()) {
+            return EMPTY;
+        }
+        final Object first = iterator.next();
+        if (!iterator.hasNext()) {
+            return String.valueOf(first);
+        }
+
+        // two or more elements
+        final StringBuilder buf = new StringBuilder(256);
+        if (first != null) {
+            buf.append(first);
+        }
+
+        while (iterator.hasNext()) {
+            if (separator != null) {
+                buf.append(separator);
+            }
+            final Object obj = iterator.next();
+            if (obj != null) {
+                buf.append(obj);
+            }
+        }
+        return buf.toString();
+    }
+
+    public static String join(final Iterable<?> iterable, final String separator) {
+        if (iterable == null) {
+            return null;
+        }
+        return join(iterable.iterator(), separator);
+    }
+
+    public static String repeat(final String str, final int repeat) {
+        if (str == null) {
+            return null;
+        }
+        if (repeat <= 0) {
+            return EMPTY;
+        }
+        final int inputLength = str.length();
+        if (repeat == 1 || inputLength == 0) {
+            return str;
+        }
+        if (inputLength == 1 && repeat <= PAD_LIMIT) {
+            return repeat(str.charAt(0), repeat);
+        }
+
+        final int outputLength = inputLength * repeat;
+        switch (inputLength) {
+            case 1 :
+                return repeat(str.charAt(0), repeat);
+            case 2 :
+                final char ch0 = str.charAt(0);
+                final char ch1 = str.charAt(1);
+                final char[] output2 = new char[outputLength];
+                for (int i = repeat * 2 - 2; i >= 0; i--, i--) {
+                    output2[i] = ch0;
+                    output2[i + 1] = ch1;
+                }
+                return new String(output2);
+            default :
+                final StringBuilder buf = new StringBuilder(outputLength);
+                for (int i = 0; i < repeat; i++) {
+                    buf.append(str);
+                }
+                return buf.toString();
+        }
+    }
+
+    public static String repeat(final char ch, final int repeat) {
+        if (repeat <= 0) {
+            return EMPTY;
+        }
+        final char[] buf = new char[repeat];
+        for (int i = repeat - 1; i >= 0; i--) {
+            buf[i] = ch;
+        }
+        return new String(buf);
     }
 
 }
